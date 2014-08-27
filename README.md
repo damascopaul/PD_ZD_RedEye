066f45119befa91f4668f4dded63874fba36cd9a
Paul Damasco <damascopaul@gmail.com>
2014-08-27 22:30:11 +0800

    Edded exception/error handling

--- README.md
+++ README.md
@@ -1,4 +1,43 @@
 slee.py
 =======
 
-Get a list of incidents in PagerDuty during off hours and create a ZenDesk ticket for incident response.
+Python script to create ZenDesk tickets from PagerDuty incidents that occurred on a certain time range. Uses ZenDesk and PagerDuty API calls.
+
+###Requirements
+
+####Python Version
+Use Python version 2.6 and later.
+
+####Python Libraries
+Make sure the following Python Libraries are installed on your system:
+* ```requests``` - available in https://github.com/kennethreitz/requests
+* ```datetime``` - new in Python version 2.3
+* ```json``` - new in Python version 2.6
+
+###Configuration
+Be sure to set the global variables before running the script.
+
+Global variable      | Description
+-------------------- | -----------
+PD_SUBDOMAIN         | PagerDuty Subdomain Name
+PD\_API\_ACCESS_KEY  | PD API Token
+ZD_SUBDOMAIN			 | ZenDesk Subdomain Name
+ZD_USER				 | ZenDesk Username
+ZD_PWD			   		 | ZenDesk Password
+ZD_REQUESTER			 | The User ID of the Requester when creating a new ticket
+ZD_GROUP				 | The Group ID of the assignee of the new ticket
+ZD_ASSIGNEE			 | The User ID of the assignee of the new ticket
+
+
+###Usage
+You can run the script on the terminal manually or run it as a cron job.
+
+####Terminal Command
+Run the script on the terminal using the command: 
+```$ python slee.py```
+You should get a terminal message if there is an error.
+
+####Cron Job
+The script can be run as a cron job. You can also pipe the message outputs to a logfile if you want.
+
+```00 08 * * * /path/to/file/slee.py >> /var/log/sleepy.log```
--- slee.py
+++ slee.py
@@ -2,6 +2,9 @@
 import requests
 import datetime
 import json
+import requests.exceptions
+
+TIME=datetime.datetime.utcnow()
 
 #PagerDuty Specific Variables
 PD_SUBDOMAIN=''
@@ -18,7 +21,7 @@ ZD_ASSIGNEE=''
 #Gets all the PagerDuty incidents 
 #from 8am yesterday to 12mn today (UTC)
 def get_incidents():
-	TIME=datetime.datetime.now()
+	data = None
 	T_SINCE=str(TIME.year)+"-"+str(TIME.month)+"-"+str(TIME.day-1)+"T08:00Z"
 	T_UNTIL=str(TIME.year)+"-"+str(TIME.month)+"-"+str(TIME.day)+"T00:00Z"
 
@@ -34,12 +37,22 @@ def get_incidents():
 	        	'sort_by':'created_on:asc',
 	        	'fields':'incident_number,created_on,service,trigger_summary_data,html_url',
     	}
-    	response = requests.get(
-	            'https://{0}.pagerduty.com/api/v1/incidents'.format(PD_SUBDOMAIN),
-	            headers=headers,
-	            params=parameters,
-    	)
-    	data = json.loads(response.text)
+    	try:
+    		response = requests.get(
+		           'https://{0}.pagerdutsy.com/api/v1/incidents'.format(PD_SUBDOMAIN),
+		           headers=headers,
+		           params=parameters,
+    		) 
+    		response.raise_for_status()
+    		data = json.loads(response.text)
+    	except requests.exceptions.ConnectionError as e:
+    		print '{1} : PD : There is a problem with your network. {0}'.format(e, TIME)
+    	except requests.exceptions.HTTPError as e:
+    		print '{1} : PD : An error occured. {0}'.format(e, TIME)
+    	except requests.packages.urllib3.exceptions.ProtocolError as e:
+    		print '{2} : PD : An Error Occured: {0} {1}'.format(e[0], e[1][1], TIME)
+    	except Exception as e:
+    		print '{1} : PD : Unexpected error: {0}'.format(e, TIME)
 
     	return data
 
@@ -62,14 +75,24 @@ def create_ticket(subject, body):
     		}
     	}
     	data = json.dumps(ticket)
-    	response = requests.post(
-	            'https://{0}.zendesk.com/api/v2/tickets.json'.format(ZD_SUBDOMAIN),
-	            headers=headers,
-	            data=data,
-	            auth=(ZD_USER, ZD_PWD),
-    	)
+    	try:
+	    	response = requests.post(
+		            'https://{0}.zendesk.com/api/v2/ticksssets.json'.format(ZD_SUBDOMAIN),
+		            headers=headers,
+		            data=data,
+		            auth=(ZD_USER, ZD_PWD),
+	    	)
+	    	response.raise_for_status()
+	except requests.exceptions.ConnectionError as e:
+    		print '{1} : ZD : There is a problem with your network. {0}'.format(e, TIME)
+	except requests.exceptions.InvalidURL as e:
+		print "{1} : ZD : {0}".format(e, TIME)
+	except requests.exceptions.HTTPError as e:
+    		print '{1} : ZD : An error occured. {0}'.format(e, TIME)
+    	except Exception as e:
+    		print '{1} : ZD : Unexpected error: {0}'.format(e, TIME)
 
-#Parses the json formatted response from the get_incidents function
+#Parses and formats the json  response from the get_incidents function
 #and creates a new ticket in ZenDesk for each incident.
 def beautify_incidents(ugly_incidents):
 	for x in range(ugly_incidents['total']):
